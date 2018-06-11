56a392e10ec14999c00f900be10b3d53094c3b13
mihneavlad <mihnel@yahoo.com>
2018-06-11 18:08:01 +0200

    First working version of the dice exercise including the provided images. Final design touches are still to be made.

--- scripts/index.js
+++ scripts/index.js
@@ -1,40 +1,40 @@
 function getRandom(min, max) {
-  min = Math.ceil(min);
-  max = Math.floor(max);
-  return Math.floor(Math.random() * (max - min + 1)) + min;
-  }
+	min = Math.ceil(min);
+	max = Math.floor(max);
+	return Math.floor(Math.random() * (max - min + 1)) + min;
+}
 
 let dieOne = {
-    value: 1,
-    roll: function() {
-        this.value = getRandom(1, 6);
-        return this.value;
-    }
+	value: 1,
+	roll: function() {
+		this.value = getRandom(1, 6);
+		return this.value;
+	}
 };
 
 let dieTwo = {
-    value: 1,
-    roll: function() {
-        this.value = getRandom(1, 6);
-        return this.value;
-    }
+	value: 1,
+	roll: function() {
+		this.value = getRandom(1, 6);
+		return this.value;
+	}
 };
 
 
 function printNumber(number1, number2) {
-  let value1 = document.querySelector('#dice1');
-  let value2 = document.querySelector('#dice2');
-  value1.innerText = number1;
-  value2.innerText = number2;
-}
 
+	let value1 = document.querySelector('#dice1');
+	let value2 = document.querySelector('#dice2');
+	value1.style['background-image'] = `url(/images/inverted-dice-${number1}.svg)`;
+	value2.style['background-image'] = `url(/images/inverted-dice-${number2}.svg)`;
+}
 
 let button = document.querySelector('button');
 
 button.addEventListener('click', function(event) {
-  event.preventDefault();
+	event.preventDefault();
 
-  let result1 = dieOne.roll();
-  let result2 = dieTwo.roll();
-  printNumber(result1, result2);
+	let result1 = dieOne.roll();
+	let result2 = dieTwo.roll();
+	printNumber(result1, result2);
 });
