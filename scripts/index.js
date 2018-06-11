f88c4cdf91f99851f1dde758415572c37360fef3
mihneavlad <mihnel@yahoo.com>
2018-06-11 16:11:48 +0200

    First working version of the dice project.

--- scripts/index.js
+++ scripts/index.js
@@ -1,1 +1,40 @@
-//Test
+function getRandom(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+  }
+
+let dieOne = {
+    value: 1,
+    roll: function() {
+        this.value = getRandom(1, 6);
+        return this.value;
+    }
+};
+
+let dieTwo = {
+    value: 1,
+    roll: function() {
+        this.value = getRandom(1, 6);
+        return this.value;
+    }
+};
+
+
+function printNumber(number1, number2) {
+  let value1 = document.querySelector('#dice1');
+  let value2 = document.querySelector('#dice2');
+  value1.innerText = number1;
+  value2.innerText = number2;
+}
+
+
+let button = document.querySelector('button');
+
+button.addEventListener('click', function(event) {
+  event.preventDefault();
+
+  let result1 = dieOne.roll();
+  let result2 = dieTwo.roll();
+  printNumber(result1, result2);
+});
